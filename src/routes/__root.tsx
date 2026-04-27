5f1f58fd7da484ca92a0b9b085104c51ffaf1fb8
Josue Blanco <josue.blanco@solvex.com.do>
2026-04-27 17:22:51 -0400

    fix: vercel config + speed insights

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import { Outlet, Link, createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import appCss from "../styles.css?url";
 
@@ -55,6 +56,7 @@ function RootShell({ children }: { children: React.ReactNode }) {
       <body>
         {children}
         <Scripts />
+        <SpeedInsights />
       </body>
     </html>
   );
